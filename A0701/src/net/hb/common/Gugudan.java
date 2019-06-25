929f41a492acf1f52a1d783cc0263fd53487a7a1
user <user@DESKTOP-37GULAI>
2019-06-25 15:49:50 +0900

    07-01-화요일 구구단 문서 03:49

---
+++ A0701/src/net/hb/common/Gugudan.java
@@ -0,0 +1,13 @@
+package net.hb.common;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+		System.out.println("Gugudan.java ");
+		System.out.println("3:46");
+		int dan=4;
+		for(int i=1; i<10; i++) {
+			System.out.println(dan + "*" + i + "=" + (dan*i));
+		}
+	}//end
+}//class end
--- A0701/src/net/hb/common/Last.java
+++ A0701/src/net/hb/common/Last.java
@@ -5,5 +5,7 @@ public class Last {
 	public static void main(String[] args) {
 		System.out.println("Last.java ");
 		System.out.println("3:35");
+		System.out.println("마지막문장 마지막문장");
+		System.out.println("git git 연습");
 	}//end
 }//class end
